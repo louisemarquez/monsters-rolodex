843f1876abda378e4c2f91e8762948103074891e
Louise Marquez <louisemarquez@MacBook-Pro.local>
2020-05-12 00:26:31 -0400

    adding files for github pages

--- src/index.js
+++ src/index.js
@@ -8,8 +8,15 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root') /*It's looking on the document object for 
+                                  for some element with ID of "root" and it's 
+                                  replacing it with this (our)function that we saw that
+                                  returns HTML */
+
 );
+
+/* That div with an ID of root is in index.html. This is the entry way
+for our application*/
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
