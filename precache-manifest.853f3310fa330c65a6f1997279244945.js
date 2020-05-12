0f22f0e220e460a8a16d183142c3de7f8afeb74e
Louise Marquez <louisemarquez@MacBook-Pro.local>
2020-05-12 00:25:41 -0400

    Updates

---
+++ precache-manifest.853f3310fa330c65a6f1997279244945.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64b5e6f800c3d29a01ecf639ac9663d0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "933d0d9d404421d61b75",
+    "url": "/monsters-rolodex/static/css/main.3c166ad2.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "933d0d9d404421d61b75",
+    "url": "/monsters-rolodex/static/js/main.26ecd12b.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
